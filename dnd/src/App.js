edcbfa9ce0831fbc1d990c10d5bd1546e8e7efa3
Thomas Barrett <tsbarrett89@gmail.com>
2019-11-09 09:43:43 -0700

    CRA and added dependencies

---
+++ dnd/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
---
+++ dnd/src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navigation = () => {
+
+    return(
+        <div>
+            <h1>Dungeons and Dragons Spell and Action Sheet</h1>
+        </div>
+    )
+}
+
+export default Navigation
